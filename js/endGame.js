eae330257e4f256e1a4c35a4e84704a5139e2156
AmyOShea <amyoshea.lsad@gmail.com>
2021-03-07 18:21:08 +0000

    Pulled scored from locals storage to create high scores. Set max diplayable high scores

--- js/endGame.js
+++ js/endGame.js
@@ -6,6 +6,8 @@ const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("save-high-score");
 const recentScore = localStorage.getItem("recentScore");
 const endScore = document.getElementById("end-score")
+const highScores = JSON.parse(localStorage.getItem("highscores")) || [];
+
 
 
 //----------------------------------------------------------------- Save/Display high scores
@@ -15,12 +17,25 @@ const endScore = document.getElementById("end-score")
 endScore.innerText = `Final Score: ${recentScore}`;
 
 
-
-//----------------------------------------------------------------- High Score Button Event
-
 function saveHighScore(event) {
-        // Prevent form reload
+        // Prevent form reload when save button clicked
     event.preventDefault();
-}
+
+        //Create key values for score
+    const score = {
+        score: recentScore,
+        name: username.value
+    };
+        //Add score to highscores array
+    highScores.push(score);
+
+        //Set max saved scores to 2 (keeping at 2 for testing, wil be changed later)
+    highScores.splice(2);
+
+        //Update highscores as a string
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+
+    console.log(highScores);
+};
 
 
