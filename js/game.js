ba963fecba8c6dec8de7a1bf5839b629f33c53d2
AmyOShea <amyoshea.lsad@gmail.com>
2021-03-08 19:21:35 +0000

    Updated max questions to 10. Deleted temporary questions and pulled full question list from db.json. set startGame frunction to run only only after questions are loaded

--- js/game.js
+++ js/game.js
@@ -5,7 +5,7 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("ans-choice"));
 const correctAnsBonus = 1;
-const maxQuestions = 3; //Keeping at 3 for tesiting functionality, will be changed to 10 later.
+const maxQuestions = 10;
 const questionCounterDisplay = document.getElementById("question-counter");
 const currentScoreDisplay = document.getElementById("current-score");
 
@@ -15,30 +15,21 @@ let acceptingAnswers = false;
 let score = 0;
 let questionCounter = [];
 
-//----------------------------------------------------------------- Temporary questions for tesiting functionality, will be removed later.
-let questions = [
-    {
-      question: "How many legs does a spider have?",
-      correct: 2,
-      ans1: 6,
-      ans2: 8,
-      ans3: 10
-    },
-    {
-      question: "What gas do we breathe to stay alive?",
-      correct: 1,
-      ans1: "Oxygen",
-      ans2: "Argon",
-      ans3: "Nitrogen"
-    },
-    {
-      question: "What is the largest country in the world?",
-      correct: 1,
-      ans1: "Russia",
-      ans2: "Italy",
-      ans3: "Australia"
-    }
-];
+//-------------------------------------------------------------------------------- Questions
+
+let questions = [];
+
+        //Calling questions stored in db.json
+fetch("db.json")
+.then( res => {
+    return res.json();
+})
+        //Setting pulled data into questions array
+.then(loadedQuesitons => {
+    questions = loadedQuesitons;
+        //Game starts AFTER questions are loaded
+    startGame();
+});
 
 
 //--------------------------------------------------------------------------------  Game Play
@@ -114,6 +105,4 @@ function updateCurrentScore(num) {
     score += num;
     currentScoreDisplay.innerText = `Score: ${score}`;
 }
-
-startGame();
 
